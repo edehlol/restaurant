9c7f21fdd7af8dab13e327d2ece7834001ac3f4b
edehlol <edymusajev@gmail.com>
2019-11-29 22:26:31 +0200

    moved img folder to dist

--- src/about.js
+++ src/about.js
@@ -6,7 +6,7 @@ const text = document.createElement('p')
 text.innerHTML = 'The place to go, if you want fast and delicious meals at a reasonable price. We use fresh ingredients and update our menu regularly depending on the season. Come by to enjoy a fantastic lunch or dinner, or order online and we\'ll have it delivered to you in no time. '
 text.setAttribute('id', 'aboutText')
 const image = document.createElement('img')
-image.setAttribute('src', '../img/food.jpg')
+image.setAttribute('src', '/img/food.jpg')
 image.setAttribute('id', 'aboutImage')
 image.style.maxWidth = '400px'
 image.style.textAlign = 'center'
